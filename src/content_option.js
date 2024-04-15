d09dfcb626714dfaf622ec77db2e80ad3c3883b9
Nicolas Mazia <nicolas.mazia@gat.digital>
2024-04-15 17:58:45 -0300

    portifolio

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logotext = "FS";
+const meta = {
+    title: "Front Sennin",
+    description: "I’m Front Sennin developer _ Full stack devloper,currently working in Brazil",
+};
+
+const introdata = {
+    title: "I’m Front Sennin",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "",
+    profile_img_url: "https://scontent.fcgh6-1.fna.fbcdn.net/v/t39.30808-6/363324935_9707967052607475_4967169803572170072_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEGMI6fe0JvzTqGcmwVOyk9kdCEx9opcbSR0ITH2ilxtDSEX03rOwJcQqGbfbg8otB8uv9WRThEUGJDWAf7vS0y&_nc_ohc=gP8MqHlE-tYAb7akVPf&_nc_oc=AdjfPWwyjln3RtRThI5M72EYfReMnt7X0ARa6iBNyTo06ERTUO6zx4PVDQzopCY2BUEPIY1EAGFyNhucgjQovxkc&_nc_ht=scontent.fcgh6-1.fna&oh=00_AfBlDXC3N8daetIIY3ghzUIPd1bIVaTOc15WiFDinZgdvQ&oe=66236DFB",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "Full Stack professional with +10 years of experience, with expertise in Angular and ReactJS, standing out in developing dynamic interfaces. Basic knowledge of Java, Node and .NET. Ability to collaborate on the back-end, but main focus is front-end. Proactive in learning and adapting quickly.",
+};
+const worktimeline = [{
+    jobtitle: "Frontend Developer",
+    where: "B3 Bank",
+    date: "November 2023 - Present",
+},
+{
+    jobtitle: "Frontend Developer",
+    where: "Anglo",
+    date: "June 2021 - February 2024",
+},
+{
+    jobtitle: "Front-End Specialist Developer",
+    where: "Grupo FCamara - SuperDiggital",
+    date: "January 2023 - September 2023",
+},
+{
+    jobtitle: "ReactJS Front-End Specialist Developer",
+    where: "Grupo FCamara - Elfa Medicamentos",
+    date: "January 2022 - January 2023",
+},
+{
+    jobtitle: "Full-Stack Tech Lead",
+    where: "Grupo FCamara - Klabin",
+    date: "January 2021 - January 2022",
+},
+{
+    jobtitle: "Front-End Developer",
+    where: "Olga Ri",
+    date: "September 2019 - December 2020",
+},
+{
+    jobtitle: "Full-Stack Angular + NodeJS Developer",
+    where: "ZRP",
+    date: "August 2018 - August 2019",
+},
+{
+    jobtitle: "Front-End Developer",
+    where: "Arizona",
+    date: "February 2016 - May 2018",
+}
+];
+
+const skills = [{
+    name: "Angular",
+    value: 99,
+},
+{
+    name: "ReactJS",
+    value: 99,
+},
+{
+    name: "Javascript",
+    value: 99,
+},
+{
+    name: "Typescript",
+    value: 99,
+},
+{
+    name: "HTML & CSS/SCSS/SASS",
+    value: 99,
+},
+{
+    name: "NodeJS",
+    value: 70,
+},
+{
+    name: "Java",
+    value: 40,
+},
+{
+    name: ".NET",
+    value: 50,
+},
+{
+    name: "Python",
+    value: 50,
+},
+];
+
+const services = [{
+    title: "Web Development",
+    description: 'Comprehensive web development tailored to your specific needs. We create and maintain websites and web applications to drive your business forward. Expertise in frontend, backend, and custom solutions.',
+},
+{
+    title: "Mobile Apps",
+    description: "Customized React Native and Ionic app development to elevate your business. From initial design to deployment, we craft mobile solutions that engage and captivate your audience. Expertise in cross-platform development and seamless user experiences.",
+},
+];
+
+const contactConfig = {
+    profile_EMAIL: "nicolas.mazia@gmail.com",
+    profile_FONE: "+55 (11) 94346-9442",
+    description: "Never call me, jusr text message or email",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    profile_SERVICE_ID: "service_id",
+    profile_TEMPLATE_ID: "template_id",
+    profile_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/frontsennin",
+    linkedin: "https://www.linkedin.com/in/nicolas-mazia-233840263/",
+    instagram: "https://www.instagram.com/front.sennin/",
+};
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
